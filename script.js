ef912aebbdc66ab95b3b40d764a192726c1d2d02
sneharajpurkar <snehasrajpurkar@gmail.com>
2023-03-02 20:54:01 +0530

    myntra

--- script.js
+++ script.js
@@ -16,7 +16,8 @@ function register(event){
         }
     }
     if(flag === true){
-        alert("Email already present, use another one")
+        window.location.href = "/register.html";
+        alert("Registration Done")
     } else if(userPassword.length < 1 && userName.length < 1 && userNumber.length < 1 && userEmail.length < 1 ){
         alert("must fill all fields")
     }else if (userPassword.length < 8 ){
@@ -29,7 +30,34 @@ function register(event){
         document.getElementById("userEmail").value =" ";
         document.getElementById("userPassword").value = " ";
         // window.location.href = "/login.html";
-        alert("Registration done");
+        alert("Email already present, use another one");
 
+    }
+}
+
+
+function login(event){
+    event.preventDefault();
+    var userEmail = document.getElementById("email").value;
+    var userPassword = document.getElementById("password").value;
+    var userData = {email : userEmail, password : userPassword}
+
+    var dataFromLS = JSON.parse(localStorage.getItem("userData")) || [];
+    console.log(dataFromLS, 'dataFromLS')
+    var flag = false;
+    for(var i = 0; i < dataFromLS.length; i++){
+        if(dataFromLS[i].email === userEmail && dataFromLS[i].password === userPassword){
+            flag = true;
+        }
+    }
+    if(flag === true){
+        window.location.href = "/index.html";
+        alert("login succecfully")
+    } else {
+        dataFromLS.push(userData);
+        localStorage.setItem("userData",JSON.stringify(dataFromLS));
+        document.getElementById("email").value =" ";
+        document.getElementById("password").value = " ";
+        alert("Wrong cred, Please check your email and password");
     }
 }
